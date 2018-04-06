cd03233779d3c5bfd0099a87929d5134951f22db
ivanhmolina <35584318+ivanhmolina@users.noreply.github.com>
2018-04-05 20:35:01 -0500

    first commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,8 +4,19 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams, $http) {
-	$scope.getWeather = function () {
-		$scope.mdl.city_name = "adios";
+	$scope.mdl={};
+	$scope.mdl.appid="2e5aa9b1bf4acbed133c258809aee5ef";
+	$scope.getWeather = function() {
+		url = "http://api.openweathermap.org/data/2.5/weather?q="+$scope.mdl.city_name+"&mode=json&APPID="+$scope.mdl.appid;
+		//http://api.openweathermap.org/data/2.5/weather?q=Mexico&mode=json&APPID=2e5aa9b1bf4acbed133c258809aee5ef
+		//url = "http://api.openweathermap.org/data/2.5/weather?q=Mexico&mode=json&APPID=2e5aa9b1bf4acbed133c258809aee5ef"; 
+		$http.get(url).then(function(response) {
+	        $scope.mdl.current_temp 	= response.data.main.temp;
+	        $scope.mdl.weather_comment 	= response.data.weather[0].description;
+	        $scope.mdl.weather_icon 	= response.data.weather[0].icon;
+	    }, function myError(response) {
+	        $scope.mdl.city_name = response.statusText;
+	    });
 	};
 
 }])
